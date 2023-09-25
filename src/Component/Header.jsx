771afbe367c80c68f7a6c4568a47d874695e4f49
pulkitBhatnagar <ipaul@liveareacx.com>
2023-09-26 00:50:53 +0530

    reverting the changes

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -56,40 +56,42 @@ const Header = () => {
 
     const googleSignInParam = new GoogleAuthProvider();
 
-    function googleSignIn() {
-      
-
-      signInWithPopup(auth, googleSignInParam)
-        .then((result) => {
-          // This gives you a Google Access Token. You can use it to access the Google API.
-        //   const credential = GoogleAuthProvider.credentialFromResult(result);
-        //   const token = credential.accessToken;
-        //   // The signed-in user info.
-        //   const user = result.user;
-          // IdP data available using getAdditionalUserInfo(result)
-          // ...
+    async function googleSignIn() {
+      await signInWithPopup(auth, googleSignInParam).then((res) => {
           handleClose();
           setAlert({
-            open: true,
-            severity: "success",
-            message: "Logged In successfully",
+              open: true,
+              severity: 'success',
+              message: 'Logged In successfully',
           });
-        })
-        .catch((error) => {
-          // Handle Errors here.
-        //   const errorCode = error.code;
-        //   const errorMessage = error.message;
-        //   // The email of the user's account used.
-        //   const email = error.customData.email;
-        //   // The AuthCredential type that was used.
-          //const credential = GoogleAuthProvider.credentialFromError(error);
-          // ...
-          setAlert({
-            open: true,
-            severity: "error",
-            message: "Somrthing went wrong",
-          });
-        });
+      });
+
+    //   signInWithPopup(auth, googleSignInParam)
+    //     .then((result) => {
+    //       // This gives you a Google Access Token. You can use it to access the Google API.
+    //       const credential = GoogleAuthProvider.credentialFromResult(result);
+    //       const token = credential.accessToken;
+    //       // The signed-in user info.
+    //       const user = result.user;
+    //       // IdP data available using getAdditionalUserInfo(result)
+    //       // ...
+    //       handleClose();
+    //       setAlert({
+    //         open: true,
+    //         severity: "success",
+    //         message: "Logged In successfully",
+    //       });
+    //     })
+    //     .catch((error) => {
+    //       // Handle Errors here.
+    //       const errorCode = error.code;
+    //       const errorMessage = error.message;
+    //       // The email of the user's account used.
+    //       const email = error.customData.email;
+    //       // The AuthCredential type that was used.
+    //       const credential = GoogleAuthProvider.credentialFromError(error);
+    //       // ...
+    //     });
     }
     return (
       <ThemeProvider theme={darkTheme}>
